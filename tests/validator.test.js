24d5bab32526fc2cb3e8ae74b7ccdb214e0ae351
PierreTDX <pierre.tondeux@gmail.com>
2026-02-06 11:56:57 +0100

    test moins de 20 ans

--- tests/validator.test.js
+++ tests/validator.test.js
@@ -1,19 +1,20 @@
 import { calculateAge } from "./../module/validator.js";
+let people20years;
+beforeEach(() => {
+    let date = new Date();
+    people20years = {
+        birth: new Date(date.setFullYear(date.getFullYear() - 20))
+    };
+})
 /**
 * @function calculateAge
 */
 describe('calculateAge Unit Test Suites', () => {
     it('should return a correct age', () => {
-        const loise = {
-            birth: new Date("11/07/1991")
-        };
-
-        expect(calculateAge(loise)).toEqual(34)
-
+        expect(calculateAge(people20years)).toEqual(20)
     })
 
     it('should throw a "missing param p" error', () => {
         expect(() => calculateAge()).toThrow("missing param p")
-
     })
 })
