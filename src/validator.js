b1a7fe9f3990664e52af7211977687c4414cebfe
PierreTDX <pierre.tondeux@gmail.com>
2026-02-06 14:42:06 +0100

    init Activite1 avec travaux du matin : copie module.js dans validator.js a completer pour finir l'activite

---
+++ src/validator.js
@@ -0,0 +1,35 @@
+/**
+ * Calculate a person's age in years.
+ *
+ * @param {object} p An object representing a person, implementing a birth Date parameter.
+ * @return {number} The age in years of p.
+ */
+export function calculateAge(p) {
+    if (!p) {
+        throw new Error("missing param p")
+    }
+    if (typeof p !== 'object') {
+        throw new Error("param p must be an object")
+    }
+    if (!p.birth) {
+        throw new Error("missing param p.birth")
+    }
+    if (!(p.birth instanceof Date)) {
+        throw new Error("p.birth must be a Date object")
+    }
+    if (isNaN(p.birth)) {
+        throw new Error("p.birth is an invalid Date")
+    }
+
+    const today = new Date();
+    let age = today.getFullYear() - p.birth.getFullYear();
+    const m = today.getMonth() - p.birth.getMonth();
+
+    if (m < 0 || (m === 0 && today.getDate() < p.birth.getDate())) {
+        age--;
+    }
+    if (age < 18) {
+        throw new Error("Age must be at least 18 years old.")
+    }
+    return age;
+}
