15dc29cf054d613c28dbb53a37667b6d230da447
PierreTDX <pierre.tondeux@gmail.com>
2026-02-19 12:28:47 +0100

    fix: init cypress with router

--- my-app/cypress/e2e/spec.cy.js
+++ my-app/cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('Home page spec', () => {
     it('deployed react app to localhost', () => {
         cy.visit('http://localhost:3000')
-        cy.contains('Registration Form')
+        cy.contains('Registered Users').should('be.visible')
     })
 })
